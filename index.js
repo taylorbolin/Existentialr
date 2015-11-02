980d21e2b0286a08b2af6fa028cea887eb48b9a3
Taylor Bolin <t.n.bolin@gmail.com>
2015-11-01 23:48:52 -0800

    css touch ups

--- index.js
+++ index.js
@@ -5,24 +5,24 @@ var bodyParser = require('body-parser');
 var app = express();
 var lifeAPI = require('./apis/life_api');
 
-lifeAPI.getLife('1 April 2012', 'female');
+// lifeAPI.getLife('1 April 2012', 'female');
 
-// app.set('view engine', 'ejs');
-// app.use(ejsLayouts);
-// app.use(express.static(__dirname + '/static'));
-// app.use(bodyParser.urlencoded({extended: false}));
+app.set('view engine', 'ejs');
+app.use(ejsLayouts);
+app.use(express.static(__dirname + '/static'));
+app.use(bodyParser.urlencoded({extended: false}));
 
-// app.use('/auth', require('./controllers/auth.js'));
-// app.use('/comments', require('./controllers/comment.js'));
-// app.use('/main', require('./controllers/main.js'));
+app.use('/auth', require('./controllers/auth.js'));
+app.use('/comments', require('./controllers/comment.js'));
+app.use('/main', require('./controllers/main.js'));
 
-// app.get('/', function(req, res) {
-//   res.render('index');
-// });
+app.get('/', function(req, res) {
+  res.render('index');
+});
 
 
 
-// var port = 3000;
-// app.listen(port, function() {
-//   console.log("You're listening to the smooth sounds of port " + port);
-// });
+var port = 3000;
+app.listen(port, function() {
+  console.log("You're listening to the smooth sounds of port " + port);
+});
